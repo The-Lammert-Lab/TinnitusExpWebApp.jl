6ea34f9ae37c891ae0a2600d5a3770a4726c1bda
Nelson Barnett <nelsonvbarnett@gmail.com>
2023-02-14 18:14:08 -0500

    Remove unnecessary outer constructor

--- app/resources/blocks/Blocks.jl
+++ app/resources/blocks/Blocks.jl
@@ -6,67 +6,11 @@ import SearchLight: AbstractModel, DbId
 
 export Block
 
-mutable struct Block <: AbstractModel
-    id::DbId
-    stim_matrix::String
-    responses::String
-    n_blocks::Integer
-    n_trials_per_block::Integer
-    stimgen::String
-    min_freq::AbstractFloat
-    max_freq::AbstractFloat
-    duration::AbstractFloat
-    n_trials::Integer
-    Fs::AbstractFloat
-    n_bins::Integer
-    min_bins::Integer
-    max_bins::Integer
-
-    # Inner constructor for argument validation and n_trials definition.
-    function Block(
-        id::DbId,
-        stim_matrix::String,
-        responses::String,
-        n_blocks::Integer,
-        n_trials_per_block::Integer,
-        stimgen::String,
-        min_freq::AbstractFloat,
-        max_freq::AbstractFloat,
-        duration::AbstractFloat,
-        Fs::AbstractFloat,
-        n_bins::Integer,
-        min_bins::Integer,
-        max_bins::Integer,
-    )
-        @assert n_blocks > 0 "`n_blocks` must be specified and greater than 0"
-        @assert n_trials_per_block > 0 "`n_trials_per_block` must be specified and greater than 0"
-        n_trials = n_blocks * n_trials_per_block
-
-        return new(
-            id,
-            stim_matrix,
-            responses,
-            n_blocks,
-            n_trials_per_block,
-            stimgen,
-            min_freq,
-            max_freq,
-            duration,
-            n_trials,
-            Fs,
-            n_bins,
-            min_bins,
-            max_bins,
-        )
-    end
-end
-
-
 """
     Block(; kwargs...)
     Block(s::SG; kwargs...) where {SG<:Stimgen}
 
-Outer constructor for Block struct.
+Constructor for Block struct.
     If a subtype of TinnitusReconstructor.Stimgen is passed, 
     its fields are incorporated into the Block struct that is generated.
     `n_blocks` and `n_trials_per_block` are required kwargs.
@@ -92,42 +36,69 @@ Outer constructor for Block struct.
 - `n_trials::Integer = n_blocks * n_trials_per_block`: Total number of trials to be completed.
         Not a valid input. The value will be inferred from `n_blocks` and `n_trials_per_block`. 
 """
-function Block(;
-    id::DbId = DbId(),
-    stim_matrix::String = "",
-    responses::String = "",
-    n_blocks::Integer = 0,
-    n_trials_per_block::Integer = 0,
-    stimgen::String = "",
-    min_freq::Real = 0,
-    max_freq::Real = 0,
-    duration::Real = 0,
-    Fs::Real = 0,
-    n_bins::Integer = 0,
-    min_bins::Integer = 0,
-    max_bins::Integer = 0,
-)
+mutable struct Block <: AbstractModel
+    id::DbId
+    stim_matrix::String
+    responses::String
+    n_blocks::Integer
+    n_trials_per_block::Integer
+    stimgen::String
+    min_freq::AbstractFloat
+    max_freq::AbstractFloat
+    duration::AbstractFloat
+    n_trials::Integer
+    Fs::AbstractFloat
+    n_bins::Integer
+    min_bins::Integer
+    max_bins::Integer
 
-    return Block(
-        id,
-        stim_matrix,
-        responses,
-        n_blocks,
-        n_trials_per_block,
-        stimgen,
-        min_freq,
-        max_freq,
-        duration,
-        Fs,
-        n_bins,
-        min_bins,
-        max_bins,
-    )
+    # Inner constructor for argument validation, n_trials definition, and defaults.
+    # TODO: Complete assertions.
+    function Block(;
+        id::DbId = DbId(),
+        stim_matrix::S = "",
+        responses::S = "",
+        n_blocks::I,
+        n_trials_per_block::I,
+        stimgen::S = "",
+        min_freq::F = 0.0,
+        max_freq::F = 0.0,
+        duration::F = 0.0,
+        Fs::F = 0.0,
+        n_bins::I = 0,
+        min_bins::I = 0,
+        max_bins::I = 0,
+    ) where {I<:Integer,F<:AbstractFloat,S<:String}
+        @assert n_blocks > 0 "`n_blocks` must be greater than 0"
+        @assert n_trials_per_block > 0 "`n_trials_per_block` must be greater than 0"
+        n_trials = n_blocks * n_trials_per_block
+
+        return new(
+            id,
+            stim_matrix,
+            responses,
+            n_blocks,
+            n_trials_per_block,
+            stimgen,
+            min_freq,
+            max_freq,
+            duration,
+            n_trials,
+            Fs,
+            n_bins,
+            min_bins,
+            max_bins,
+        )
+    end
 end
 
 function Block(s::SG; kwargs...) where {SG<:Stimgen}
     # typeof(s) returns CharacterizeTinnitus.TinnitusReconstructor.XXXXX
-    return Block(; stimgen = string(split.(string(typeof(s)), '.')[end]), ntfromstruct(s)..., kwargs...)
+    return Block(;
+        stimgen = string(split.(string(typeof(s)), '.')[end]),
+        ntfromstruct(s)...,
+        kwargs...,
+    )
 end
 
 end
