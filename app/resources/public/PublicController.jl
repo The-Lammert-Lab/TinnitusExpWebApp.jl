b4ec3b1e02e24e617943aa9a34a82d0120133975
Saurabh Pande <saurabhpande74@gmail.com>
2024-05-09 20:44:24 -0400

    Add Calibration Interface

--- app/resources/public/PublicController.jl
+++ app/resources/public/PublicController.jl
@@ -3,6 +3,14 @@ module PublicController
 using CharacterizeTinnitus
 using Genie.Renderers, Genie.Renderers.Html
 using Genie.Router, Genie.Requests
+using Genie.Renderers.Json
+using GenieAuthentication
+using WAV: wavwrite
+using Base64
+using Genie.Exceptions
+using GenieSession
+using TinnitusReconstructor
+
 using GenieAuthentication
 
 function index()
@@ -18,6 +26,17 @@ end
 function lab()
     is_admin = admin_status()
     html(:public, :lab; is_admin)
+end
+
+function calibrate()
+    pure_tone_wav = pure_tone(1000, 1, 44100)
+
+    buf = Base.IOBuffer()
+    wavwrite(pure_tone_wav, buf; Fs=44100.0)
+    pure_tone_wav = base64encode(take!(buf))
+    close(buf)
+
+    html(:public, :calibrate; pure_tone_wav)
 end
 
 admin_status() = return current_user() isa Nothing ? nothing : current_user().is_admin
